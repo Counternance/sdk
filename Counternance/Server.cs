c42620cc74dd19001c4889f630f1d2ba4fc443db
Isaac Walker-Cox <isaac@Isaacs-MacBook-Pro.local>
2020-04-10 14:49:34 +0100

    Initial commit for Counternance

---
+++ Counternance/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Counternance
+{
+    public class Server
+    {
+        private System.Net.HttpListener Instance { set; get; }
+        /// <summary>
+        /// Get or set the listening state of this Server
+        /// </summary>
+        public bool Listening { set; get; } = true;
+        /// <summary>
+        /// Constructs a new Counternance Server instance
+        /// </summary>
+        public System.Security.Cryptography.X509Certificates.X509Certificate2Collection AuthenticationCertificates { get; } = new System.Security.Cryptography.X509Certificates.X509Certificate2Collection(); 
+        public Server() {
+           Instance = new System.Net.HttpListener();
+           Instance.Prefixes.Add("https://localhost/");
+           while (Listening)
+            {
+                Instance.GetContextAsync().Result.Request.GetClientCertificate();
+            }
+            
+    }
+
+    
+}
---
+++ Counternance/Webhook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Counternance
+{
+    public class Webhook
+    {
+        public string URL { set; internal get; }
+        private System.Net.Http.HttpClient client;
+        public Webhook()
+        {
+
+            client = new System.Net.Http.HttpClient();
+        }
+        /// <summary>
+        /// Poll a webhook with a standard GET request
+        /// </summary>
+        /// <returns>Whether a successful request was made</returns>
+        public bool Poll()
+        {
+          
+            return client.GetAsync(URL).Result.IsSuccessStatusCode;
+
+        }
+        /// <summary>
+        /// Poll a webhook with a specified request
+        /// </summary>
+        /// <param name="requestMessage">The request you want to send</param>
+        /// <returns>Whether a successful request was made</returns>
+        public bool Poll(System.Net.Http.HttpRequestMessage requestMessage)
+        {
+            return client.SendAsync(requestMessage).Result.IsSuccessStatusCode;
+        }
+    }
+}
